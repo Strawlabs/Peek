0349e04ac4a6699981d0dda9fac1088377681e65
Aswini M <ammu2406sm@gmail.com>
2026-07-30 21:31:43 +0530

    Remove unused supabase import in ProxyPlaygroundScreen

--- src/components/ProxyPlaygroundScreen.tsx
+++ src/components/ProxyPlaygroundScreen.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useAppState } from '../context/StateContext';
-import { supabase } from '../lib/supabase';
 
 export const ProxyPlaygroundScreen: React.FC = () => {
   const { providers, routeGatewayRequest, apiKeys } = useAppState();
